48d313e03c9b7f3be09029d785bac1ed31c1b11e
Samuel Viviescas <1006106025@u.icesi.edu.co>
2024-06-06 09:10:48 -0500

    user information displayed in profile

--- app/src/main/java/com/example/mealmate/view/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/mealmate/view/fragments/ProfileFragment.kt
@@ -1,21 +1,33 @@
 package com.example.mealmate.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mealmate.databinding.ProfileFragmentBinding
+import com.example.mealmate.domain.model.CurrentSession
+import org.checkerframework.checker.units.qual.Current
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: ProfileFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val binding: ProfileFragmentBinding = ProfileFragmentBinding.inflate(inflater, container, false)
+    ): View {
+        binding = ProfileFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val currentUser = CurrentSession.currentUser
+                binding.nameTV.text = currentUser.username
+                binding.emailTV.text = currentUser.email
     }
 
     // Método estatico para generar instancias del propio fragment
@@ -24,26 +36,5 @@ class ProfileFragment : Fragment() {
             return ProfileFragment()
         }
     }
+
 }
-/*
-
-    val binding by lazy {
-        ActivityProfileBinding.inflate(layoutInflater)
-    }
-
-    val viewmodel: ProfileViewModel by viewModels()
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        viewmodel.observeUser()
-
-        viewmodel.userState.observe(this) {
-            binding.emailTV.text = it.email
-            binding.nameTV.text = it.name
-        }
-
-
-    }
-*/
